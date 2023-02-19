013148e96c2cdbcaeeb8eb2fde98d5b6a79d4f17
aoneahsan <aoneahsan@gmail.com>
2023-02-19 17:09:30 +0500

    done with the class-transform package also looked into the class-validator package (i did not installed it though, so yes that can be used in a class based development flow for validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
+
 import { ProjectInput } from './components/project-input';
 import { ProjectList } from './components/project-list';
 import { EProjectStatus } from './utils/enums';
@@ -8,3 +11,28 @@ import { EProjectStatus } from './utils/enums';
 new ProjectInput('project-input', 'app');
 new ProjectList('project-list', 'app', EProjectStatus.active);
 new ProjectList('project-list', 'app', EProjectStatus.finished);
+
+// testing with "class-transform" package
+class User {
+	constructor(public id: string, public name: string, public age: number) {}
+
+	getInfo() {
+		return { id: this.id, name: this.name, age: this.age };
+	}
+}
+
+const usersData = [
+	{ id: 1, name: 'ahsan', age: 20 },
+	{ id: 1, name: 'ahsan', age: 20 },
+	{ id: 1, name: 'ahsan', age: 20 },
+	{ id: 1, name: 'ahsan', age: 20 },
+	{ id: 1, name: 'ahsan', age: 20 },
+	{ id: 1, name: 'ahsan', age: 20 },
+];
+
+const formattedUsersData = plainToInstance(User, usersData);
+
+for (let i = 0; i < formattedUsersData.length; i++) {
+	const element = formattedUsersData[i];
+	console.log({ element, info: element.getInfo() });
+}
